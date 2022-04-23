bc9b05ffb8e03016df8a6d85ae4e24aef3b33575
nazmulmilon <milonsarker73@gmail.com>
2022-04-23 18:02:18 +0600

    running the car from one side to another side

--- self-driving-car/main.py
+++ self-driving-car/main.py
@@ -6,13 +6,18 @@ track = pygame.image.load('track1.png')
 
 car = pygame.image.load('tesla.png')
 car = pygame.transform.scale(car, (30, 60))
-
+car_x = 155
+car_y = 300
 drive = True
+clock = pygame.time.Clock()
 while drive:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             drive = False
 
+    car_y = car_y - 2
+
     window.blit(track, (0,0))
-    window.blit(car, (155, 300))
+    window.blit(car, (car_x, car_y))
     pygame.display.update()
