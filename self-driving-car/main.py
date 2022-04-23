c8a833eecf42d31336549edd9519af6651dc7de4
nazmulmilon <milonsarker73@gmail.com>
2022-04-23 19:57:14 +0600

    turn down and rotate the car in y-axis and move forward with camera detection

--- self-driving-car/main.py
+++ self-driving-car/main.py
@@ -2,13 +2,14 @@
 import pygame
 pygame.init()
 window = pygame.display.set_mode((1200, 400))
-track = pygame.image.load('track3.png')
+track = pygame.image.load('track4.png')
 
 car = pygame.image.load('tesla.png')
 car = pygame.transform.scale(car, (30, 60))
 car_x = 155
 car_y = 300
 camera_x_offset = 0
+camera_y_offset = 0
 focal_distance = 25
 direction = 'up'
 
@@ -22,10 +23,13 @@ while drive:
 
     # detect road
     camera_x = car_x + camera_x_offset + 15
-    camera_y = car_y + 15
+
+    camera_y = car_y + camera_y_offset + 15
+
     up_px = window.get_at((camera_x, camera_y - focal_distance))[0]
+    down_px = window.get_at((camera_x, camera_y + focal_distance))[0]
     right_px = window.get_at((camera_x + focal_distance, camera_y))[0]
-    print(up_px, right_px)
+    print(up_px, right_px, down_px)
 
     # change direction or take turn
     if direction == 'up' and up_px != 255 and right_px == 255:
@@ -33,11 +37,20 @@ while drive:
         camera_x_offset = 30
         car = pygame.transform.rotate(car, -90)
 
+    elif direction == 'right' and right_px !=255 and down_px == 255:
+        direction = 'down'
+        car_x = car_x + 30
+        camera_x_offset = 0
+        camera_y_offset = 30
+        car = pygame.transform.rotate(car, -90)
+
     # drive
     if direction == 'up' and up_px == 255:
             car_y = car_y - 2
-    if direction == 'right' and right_px == 255:
-        car_x = car_x +2
+    elif direction == 'right' and right_px == 255:
+        car_x = car_x + 2
+    elif direction == 'down' and down_px == 255:
+        car_y = car_y +2
 
     window.blit(track, (0,0))
     window.blit(car, (car_x, car_y))
