d1fbf434441c0cbf452bebbd6eb924e1607d1822
nazmulmilon <milonsarker73@gmail.com>
2022-04-23 19:43:40 +0600

    turn right and rotate the car clockwise and move forward

--- self-driving-car/main.py
+++ self-driving-car/main.py
@@ -2,13 +2,16 @@
 import pygame
 pygame.init()
 window = pygame.display.set_mode((1200, 400))
-track = pygame.image.load('track1.png')
+track = pygame.image.load('track3.png')
 
 car = pygame.image.load('tesla.png')
 car = pygame.transform.scale(car, (30, 60))
 car_x = 155
 car_y = 300
+camera_x_offset = 0
 focal_distance = 25
+direction = 'up'
+
 drive = True
 clock = pygame.time.Clock()
 while drive:
@@ -18,12 +21,23 @@ while drive:
             drive = False
 
     # detect road
-    camera_x = car_x + 15
+    camera_x = car_x + camera_x_offset + 15
     camera_y = car_y + 15
     up_px = window.get_at((camera_x, camera_y - focal_distance))[0]
-    print(up_px)
-    if up_px == 255:
+    right_px = window.get_at((camera_x + focal_distance, camera_y))[0]
+    print(up_px, right_px)
+
+    # change direction or take turn
+    if direction == 'up' and up_px != 255 and right_px == 255:
+        direction = 'right'
+        camera_x_offset = 30
+        car = pygame.transform.rotate(car, -90)
+
+    # drive
+    if direction == 'up' and up_px == 255:
             car_y = car_y - 2
+    if direction == 'right' and right_px == 255:
+        car_x = car_x +2
 
     window.blit(track, (0,0))
     window.blit(car, (car_x, car_y))
